f72b7529dbde7196d2a5072c95c57efc3683fa9e
Zachary Wasserman <zachwass2000@gmail.com>
2013-01-18 22:37:55 -0500

    Better logging

--- backend.rb
+++ backend.rb
@@ -6,19 +6,17 @@ Pusher.app_id = '35601'
 Pusher.key = 'bd45d2f7ca11c6ced15e'
 Pusher.secret = '7e9f6b9c0d79d2afc994'
 
-post '/' do
-  puts 'Post Hit'
+set :logging, true
+
+post '/pusher/hook' do
   'Hello Post World'
 end
 
 get '/' do
-  puts 'Hit'
   send_file 'index.html'
 end
 
 post '/pusher/auth' do
-  puts 'Pusher hit auth'
   auth = Pusher[params[:channel_name]].authenticate(params[:socket_id])
-  puts auth.to_json
   auth.to_json
 end
